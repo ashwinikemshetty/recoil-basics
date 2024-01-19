9327b95b0c6220e80aa3e3d7beb768c920de4b29
Ashwini Kemshetty <21ashwinirao@gmail.com>
2024-01-19 15:32:35 +0530

    recoil atoms

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,39 @@
+import { RecoilRoot, useRecoilValue } from "recoil";
 import "./App.css";
+import {
+  jobsAtom,
+  messagingAtom,
+  networkAtom,
+  notificationsAtom,
+} from "./recoil/atoms";
 
 function App() {
-  return <></>;
+  return (
+    <RecoilRoot>
+      <MainApp />
+    </RecoilRoot>
+  );
+}
+
+function MainApp() {
+  const networkCount = useRecoilValue(networkAtom);
+  const jobsCount = useRecoilValue(jobsAtom);
+  const notificationsCount = useRecoilValue(notificationsAtom);
+  const messagingCount = useRecoilValue(messagingAtom);
+
+  const totalCount =
+    networkCount + jobsCount + notificationsCount + messagingCount;
+
+  return (
+    <>
+      <button>Home</button>
+      <button>My network ({networkCount > 99 ? "99+" : networkCount})</button>
+      <button>Jobs ({jobsCount})</button>
+      <button>Messaging ({messagingCount})</button>
+      <button>Notifications ({notificationsCount})</button>
+      <button>Me ({totalCount})</button>
+    </>
+  );
 }
 
 export default App;
